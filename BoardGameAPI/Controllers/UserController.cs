a23e90dedee72f2f730b7a245210eca793ebd7ad
Terence Qu <therealterencequ@gmail.com>
2024-01-01 12:04:32 +1300

    Initial commit

---
+++ BoardGameAPI/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGameAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+
+}
---
+++ BoardGameAPI/Data/User.cs
@@ -0,0 +1,8 @@
+﻿namespace BoardGameAPI.Data;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string? Email { get; set; }
+    public string? PasswordHash { get; set; }
+}
